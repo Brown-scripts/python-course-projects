a4be255bd4d4942aeedf3fbaf087ae7f539f1008
Nicole Brown <nikkibrowm299@gmail.com>
2023-01-19 09:38:52 +0000

    'trial'

---
+++ practice.py
@@ -0,0 +1,48 @@
+import time
+current_time = time.localtime()
+hour = current_time.tm_hour
+minute = current_time.tm_min
+print(hour,':',minute)
+if hour>8 or (hour==8 and minute>45):print('''It's time to wake up\nArise and Shine\nThe new day holds alot of good things for you''')
+else:
+    print('Go back bed')
+    print('Keep resting')
+count = 0
+while count < 7:
+    print('Inside loop')
+    count = count+1
+    if count == 3:
+        break
+print('Outside loop')
+
+times_value= 3
+for count in range(1,13):
+    if count==10:print('Tres bien')
+    result = times_value*count
+    print(count,'*',times_value,'=',result)
+    
+for every_letter in 'my name is naa':
+    print(every_letter)
+    
+def greeter():
+    print('hello love')
+    
+def morning_message():
+    print('hola bebe,how are you doing?')
+    
+def sen_1():
+    print('nicole')
+    
+def sen_2():
+    print('is a')
+    
+def sen_3():
+    sen_1()
+    sen_2()
+    print('girl')
+    
+
+
+
+
+    
