e1488374bed2e3a693cb3fa065a175885847ce0d
Nicole Brown <nikkibrowm299@gmail.com>
2023-02-16 08:13:23 +0000

    'try'

--- BROWN_10980830.cpp
+++ BROWN_10980830.cpp
@@ -1,71 +1,63 @@
+//BROWN KATHLEEN NICOLE
+//10980830
 #include<iostream>
+#include <cstdlib>
+#include <cstdio>
 using namespace std;
 int main(){
     //collecting student name
     string studentname;
-    //for (string i =0; i<)
     cout<<"student name is ";
     cin>>studentname;
-    cout<<studentname;
-    cout<<"\n";
+    cout<<studentname<<endl;
     //collecting scores for test 1 to test 5
-    int test1,test2,test3,test4,test5;
-    cout<<"test 1 grade is ";
+    int test1,test2,test3,test4,test5,sum,average;
+    cout<<"test 1: ";
     cin>>test1;
-    cout<<test1;
-    cout<<"\n";
-    cout<<"test 2 grade is ";
+    cout<<"test 2: ";
     cin>>test2;
-    cout<<test2;
-    cout<<"\n";
-    cout<<"test 3 grade is ";
+    cout<<"test 3: ";
     cin>>test3;
-    cout<<test3;
-    cout<<"\n";
-    cout<<"test 4 grade is ";
+    cout<<"test 4: ";
     cin>>test4;
-    cout<<test4;
-    cout<<"\n";
-    cout<<"test 5 grade is ";
+    cout<<"test 5: ";
     cin>>test5;
-    cout<<test5;
-    cout<<"\n";
     //finding the sum of the test scores
-    int sum;
     sum = test1 + test2 + test3 + test4 + test5;
     cout<<"the sum of the 5 tests is ";
     cout<<sum;
     cout<<"\n";
     //calculating the average grade
-    int average;
     average = sum/5;
-    cout<<"the average is ";
+    cout<<"The average is ";
     cout<<average;
     cout<<"\n";
     //grading the student
-    if (average>=90){
-        cout<<"A";
-        cout<<" excellent";
+    switch(average){
+        case 100:;
+        case 90:;
+        cout<<"A\n";
+        cout<<"excellent";
+        break;
+        case 80:;
+        cout<<"B\n";
+        cout<<"very good";
+        break;
+        case 70:;
+        cout<<"C\n";
+        cout<<"good";
+        break;
+        case 60:;
+        cout<<"D\n";
+        cout<<"pass";
+        break;
+        case 50:;
+        case 40:;
+        case 30:;
+        case 20:;
+        case 10:;
+        cout<<"F\n";
+        cout<<"fail";
+        break;
     }
-    if (average>=80 && average<90){
-        cout<<"B";
-        cout<<" very good";
-
-    }
-    if (average>=70 && average<80){
-        cout<<"C";
-        cout<<" good";
-
-    }
-    if (average>=60 && average<70){
-        cout<<"D";
-        cout<<" pass";
-
-    }
-    if (average<60){
-        cout<<"F";
-        cout<<" fail";
-
-    }
-
 }
---
+++ c++ practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter any character of your choice: ";
+    cin>>ch;
+    ch++;
+    cout<<"Next character after "<<ch<<" is "<<ch;
+    return 0;
+    char c;
+    cout << "Enter a character: ";
+    cin >> c;
+    cout << "ASCII Value of " << c << " is " << int(c);
+    return 0;
+}
